3876f7b3da1a78422c47bb0a49bb3e213f7ebf57
Wilmarie Faye Becbec <wilmariefaye@gmail.com>
2017-09-28 13:06:41 +0800

    Add files via upload
    Login View

--- App.js
+++ App.js
@@ -1,29 +1,37 @@
 import React from 'react';
-import {  View, Text } from 'react-native';
-import axios from 'axios';
-
-//const api = 'http://192.168.0.23:3009';
+import { FlatList, DrawerLayoutAndroid, Modal, StyleSheet, Switch, ToastAndroid, View } from 'react-native';
+import { Button, FormInput, FormLabel, Header, Icon, List, ListItem, Text } from 'react-native-elements';
 
 export default class App extends React.Component {
-  constructor(props, ctx){
+  constructor(props, ctx) {
     super(props, ctx);
 
+    this.handlePressLogin = this.handlePressLogin.bind(this);
+    this.handlePressRegister = this.handlePressRegister.bind(this);
+
+    this.state = {
+
+    };
   }
 
-  render(){
-    return(
-      <View style={styles.mainView}>
-        <Text>Hello there</text>
-      </View>       
+  handlePressLogin(){
+
+  }
+
+  handlePressRegister(){
+
+  }
+
+  render() {
+    return (
+      <View  style={{justifyContent: 'center', alignItems: 'center', top: 300}}>
+        <FormLabel>Username</FormLabel>
+        <FormInput onChangeText={text => this.setState({ usernameInput: text })} value={this.state.usernameInput} />
+        <FormLabel>Password</FormLabel>
+        <FormInput onChangeText={text => this.setState({ passwordInput: text })} value={this.state.passwordInput} />
+        <Button onPress={this.handlePressLogin} buttonStyle={{width: 321, marginBottom: 10, marginTop: 10}} title='Login'/>
+        <Button onPress={this.handlePressRegister} buttonStyle={{width: 321}} title='Register'/>
+      </View>
     );
   }
-
 }
-
-
-const styles = StyleSheet.create({
-    mainView: {
-      flex: 1,
-      marginTop: 70
-    }
-})
